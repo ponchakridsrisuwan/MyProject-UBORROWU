7f43c1ea7e92bb6fbdb9516a0ca92693228c6b21
wanx-wan <upamaiwancharoen@gmail.com>
2023-01-28 20:13:06 +0700

    delete django extension

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'allauth.account', #new
     'allauth.socialaccount', #new
     'allauth.socialaccount.providers.google', #new
-    'django_extensions', 
     'myapp',
     'myappstaff',
     'myappSuper',
