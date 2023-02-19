7f935a350220d10559da4633d7018fd557835b65
wanx-wan <upamaiwancharoen@gmail.com>
2023-02-20 00:23:57 +0700

    SessionTimeout

--- myapp/task.py
+++ myapp/task.py
@@ -4,26 +4,8 @@ from django.utils import timezone
 from .models import CartParcel
 from django.contrib.auth.models import User   
 
-app = Celery('tasks', broker='redis://localhost:6379/0')
-
 @task
 def remove_expired_cart_items():
     now = timezone.now()
     expired_cart_items = CartParcel.objects.filter(created_at__lte=now - timedelta(minutes=1))
     expired_cart_items.delete()
-
-
-@app.task
-def update_user_status():
-    users = User.objects.filter(status="ถูกจำกัดสิทธิ์")
-    for user in users:
-        if user.deadline <= datetime.now():
-            user.status = "ปกติ"
-            user.save()
-
-app.conf.beat_schedule = {
-    "update_user_status": {
-        "task": "tasks.update_user_status",
-        "schedule": 3600.0,
-    },
-}    
---
+++ mywebsite/middlewares.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.conf import settings
+
+class SessionTimeoutMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, req):
+        if req.user.is_authenticated and req.session.get_expiry_age() <= 0:
+            return redirect(reverse('/'))
+
+        response = self.get_response(req)
+
+        return response
