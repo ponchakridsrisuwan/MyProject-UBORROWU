7f3a74a72335b076820682b258376e3b865c61f4
wanx-wan <upamaiwancharoen@gmail.com>
2023-03-01 01:25:45 +0700

    admin.py

--- myappSuper/admin.py
+++ myappSuper/admin.py
@@ -2,7 +2,9 @@ from datetime import *
 from django.contrib import admin
 from myappSuper.models import *
 from allauth.socialaccount.models import SocialAccount
-from django.contrib.auth.models import Group, User        
+from django.contrib.auth.models import Group, User   
+from django.db.models.signals import pre_save
+from django.dispatch import receiver     
 
 RIGHT = (
         ('ผู้ดูแลระบบ', 'ผู้ดูแลระบบ'),
@@ -15,10 +17,16 @@ STATUS = (
         ('ถูกจำกัดสิทธิ์', 'ถูกจำกัดสิทธิ์'),
     )
 
-User.add_to_class('right', models.CharField(max_length = 100, choices = RIGHT, default = 'ผู้ดูแลระบบ'))
+User.add_to_class('right', models.CharField(max_length = 100, choices = RIGHT, default = 'นักศึกษา'))
 User.add_to_class('status', models.CharField(max_length = 100, choices = STATUS, default = 'ปกติ'))
 User.add_to_class('phone', models.CharField(max_length = 10, null=True, blank=True))
 User.add_to_class('reasonfromstaff', models.CharField(max_length = 254, null=True, blank=True))
 User.add_to_class('datestatus', models.DateTimeField(auto_now_add=True, null=True, blank=True))
 User.add_to_class('deadline', models.DateTimeField(auto_now_add=False, null=True, blank=True))
 User.add_to_class('token', models.CharField(max_length = 254, null=True, blank=True))
+
+
+@receiver(pre_save, sender=User)
+def set_user_right(sender, instance, **kwargs):
+    if instance.email.endswith('wancharoen.up.63@ubu.ac.th'):
+        instance.right = 'ผู้ดูแลระบบ'
